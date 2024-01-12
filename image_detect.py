5b939386e4722107968a1ad2ac5f5ecf8d667949
JonneSaloranta <saloranta.jonne@gmail.com>
2024-01-12 08:51:24 +0200

    update

--- image_detect.py
+++ image_detect.py
@@ -1,5 +1,7 @@
 from ultralytics import YOLO
 
-model = YOLO("./best.pt")  # oma malli
+model = YOLO("./best.pt")  # oma malli tai jokin muu malli esim, yolov8n.pt
 
-results = model("./test_img/cat_6.jpeg")  # pass the frame to the model
+url = 'https://www.youtube.com/watch?v=LJvyb_WujZg'
+
+results = model(url)  # pass the frame to the model
