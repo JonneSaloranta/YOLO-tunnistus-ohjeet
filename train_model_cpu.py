ebb3878cb6cf834d05da5ad8fca727f9dee88df1
JonneSaloranta <saloranta.jonne@gmail.com>
2024-01-12 08:49:40 +0200

    update

--- train_model_cpu.py
+++ train_model_cpu.py
@@ -3,7 +3,7 @@ from ultralytics import YOLO
 model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
 
 try:
-    results = model.train(data="dataset/dataset.yaml", epochs=10, batch=8, imgsz=640, workers=4)
+    results = model.train(data="dataset/dataset.yaml", epochs=200, imgsz=640)
 
 except Exception as e:
     print(e)
